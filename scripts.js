27344884b8e115ec49ba5c2c20d803afbe1aab4c
CraigBertke <73251745+CraigBertke@users.noreply.github.com>
2021-02-02 12:28:02 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -28,15 +28,9 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log("it worked!")
         //Config the Model
         var newResource = {
-            description: document.getElementById("description").value, //Put the item description here
-            qty: document.getElementById("qty").value, //Put the qty of items here 
-			maxQty: document.getElementById("qty").value,
-            name: document.getElementById("name").value, //Name of the item here
-            ownerLocation: document.getElementById("location").value, //Location of owner here
-            ownerName: document.getElementById("ownerName").value, //Name of owner here
-			borrower: "STILL AVAILABLE", //Name of borrower empty
-			checkOut: new Date().toString(),
-			checkIn: new Date().toString(),
+            skill: document.getElementById("Skill").value, //Put the Skill name
+            
+            Discription: document.getElementById("description").value, //Skill discritopn here
         };
     
         resourceRef.push(newResource);
